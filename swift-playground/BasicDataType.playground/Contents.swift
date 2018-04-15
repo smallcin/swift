3cd90dc2bb6f76a79e6971b7c29d74ca4ffcff00
zh_wj <759266178@qq.com>
2018-04-15 21:13:20 +0800

    [章节]:1.1-2.2 [简述]:swift基本语法

---
+++ swift-playground/BasicDataType.playground/Contents.swift
@@ -0,0 +1,28 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+//数学进制与计算机存储原理
+var type_10 = 17;
+var type_2 = 0b10001;
+var type_8 = 0o21;
+var type_16 = 0x11;
+
+/*
+ 计算机内存中最小的数据运算单元是一个二进制位（bit），0或1
+ 一个字节由8个二进制运算位组成
+ 针对无符号数来说，1个字节最大可以表示的数为二进制1111111即，十进制的255
+ ASCII码的存储空间即为一个字节的大小，因此最多可以表示256个字符
+ 还有KB、MB、GB、TB
+ 1B = 8bit
+ 1KB = 2^10B
+ 1MB = 2^10KB
+ 1GB = 2^10MB
+ 1TB = 2^10GB
+ 1PB = 2^10TB
+ */
+
+
+
+
---
+++ swift-playground/MyPlayground.playground/Contents.swift
@@ -0,0 +1,16 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+//常量 let 变量 var
+
+var strName:String //类型安全语言，变量和常量声明的时候必须明确类型
+strName = "Hello World!"
+print(strName)
+
+strName = strName + "add"
+
+var 你好 = "nihao"//可以汉子、表情
+print(你好)
